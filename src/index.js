f4e1131ac0966cf1c5d9453323e4581dd3d3f5cc
seangaug <seanjnr1991@aol.com>
2023-02-17 16:10:17 +0000

    associations & subsequent tests added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const {User} = require('./User');
 const {Board} = require('./Board');
 const {Cheese} = require('./Cheese');
 
-// define relationship between models
+// Users/Boards Association
+Board.belongsTo(User);
+User.hasMany(Board);
+
+// Boards/Cheeses Association
+Board.belongsToMany(Cheese, {through: 'BoardCheese'});
+Cheese.belongsToMany(Board, {through: 'BoardCheese'});
 
 module.exports = {
     User,
--- src/index.test.js
+++ src/index.test.js
@@ -6,7 +6,7 @@ describe('User, Cheese, and Board models', () => {
         await sequelize.sync({force: true});
     });
 
-    it('can create a User', async () => {
+    test('can create a User', async () => {
         const user1 = await User.create({
             name: 'John Doe',
             email: 'johndoe@email.com'
@@ -15,7 +15,7 @@ describe('User, Cheese, and Board models', () => {
         expect(user1.email).toBe('johndoe@email.com');
     });
 
-    it('can create a Cheese', async () => {
+    test('can create a Cheese', async () => {
         const cheese1 = await Cheese.create({
             title: 'Cheddar',
             description: 'Aged cheddar cheese'
@@ -24,7 +24,7 @@ describe('User, Cheese, and Board models', () => {
         expect(cheese1.description).toBe('Aged cheddar cheese');
     });
 
-    it('can create a Board', async () => {
+    test('can create a Board', async () => {
         const board1 = await Board.create({
             type: 'Hard',
             description: 'A hard cheese board',
@@ -33,5 +33,63 @@ describe('User, Cheese, and Board models', () => {
         expect(board1.type).toBe('Hard');
         expect(board1.description).toBe('A hard cheese board');
         expect(board1.rating).toBe(5);
+    });
+});
+
+describe('User, Cheese, and Board associations', () => {
+    beforeAll(async () => {
+        await sequelize.sync({force: true});
+    });
+
+    test('can associate a User with a Board', async () => {
+        const user2 = await User.create({
+            name: 'Thomas Barker',
+            email: 'tombarker@email.com'
+        });
+        const board2 = await Board.create({
+            type: 'Soft',
+            description: 'A soft cheese board',
+            rating: 4
+        });
+
+        //add board2 to user2
+        await user2.addBoard(board2);
+
+        //get all boards associated with user2
+        const userBoards = await user2.getBoards();
+
+        //check that userBoards is an array
+        expect(userBoards[0].type).toBe('Soft');
+        expect(userBoards[0].description).toBe('A soft cheese board');
+        expect(userBoards[0].rating).toBe(4);
+    });
+
+    test('can create board with multiple cheeses and vice versa', async () => {
+        const board3 = await Board.create({
+            type: 'Hard',
+            description: 'A hard cheese board',
+            rating: 5
+        });
+        const cheese2 = await Cheese.create({
+            title: 'Brie',
+            description: 'A soft cheese'
+        });
+        const cheese3 = await Cheese.create({
+            title: 'Gouda',
+            description: 'A hard cheese'
+        });
+
+        //add cheese2 and cheese3 to board3
+        await board3.addCheese(cheese2);
+        await board3.addCheese(cheese3);
+
+        //get all cheeses associated with board3
+        const boardCheeses = await board3.getCheeses();
+
+        //check that boardCheeses is an array
+        expect(boardCheeses[0].title).toBe('Brie');
+        expect(boardCheeses[0].description).toBe('A soft cheese');
+        expect(boardCheeses[1].title).toBe('Gouda');
+        expect(boardCheeses[1].description).toBe('A hard cheese');
     });
 });
